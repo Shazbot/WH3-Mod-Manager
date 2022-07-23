47c9ad9db60c8eb9a59ee21425359779e7c0904d
Tibor Ilić <tilic5@gmail.com>
2022-07-23 17:47:42 +0200

    add delay when saving config

--- src/appConfigFunctions.ts
+++ src/appConfigFunctions.ts
@@ -1,9 +1,20 @@
 import { app } from "electron";
 import * as fs from "fs/promises";
 
+let saveConfigTimeout: NodeJS.Timeout;
+let saveData: AppState | undefined;
+
 export function saveAppConfig(data: AppState) {
-  const userData = app.getPath("userData");
-  fs.writeFile(`${userData}\\config.json`, JSON.stringify(data));
+  saveData = data;
+  if (saveConfigTimeout) {
+    saveConfigTimeout.refresh();
+  } else {
+    saveConfigTimeout = setTimeout(() => {
+      console.log("WRITING CONFIG");
+      const userData = app.getPath("userData");
+      fs.writeFile(`${userData}\\config.json`, JSON.stringify(saveData));
+    }, 250);
+  }
 }
 
 export async function readAppConfig(): Promise<AppState> {
