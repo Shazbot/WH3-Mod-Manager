7eb3974536efa90a6e58f0a504d0fcc9b99384dc
Tibor Ilić <tilic5@gmail.com>
2022-09-13 21:11:38 +0200

    write config to a temp file and then replace files

--- src/appConfigFunctions.ts
+++ src/appConfigFunctions.ts
@@ -1,6 +1,7 @@
 import { app } from "electron";
 import * as fs from "fs/promises";
 import equal from "fast-deep-equal";
+import { move } from "fs-extra";
 
 let writeConfigTimeout: NodeJS.Timeout;
 let dataToWrite: AppStateToWrite | undefined;
@@ -28,14 +29,22 @@ export function writeAppConfig(data: AppState) {
   const toWrite: AppStateToWrite = appStateToConfigAppState(data);
 
   if (equal(dataToWrite, toWrite)) return;
+  const userData = app.getPath("userData");
 
   dataToWrite = toWrite;
   if (writeConfigTimeout) {
     writeConfigTimeout.refresh();
   } else {
-    writeConfigTimeout = setTimeout(() => {
-      const userData = app.getPath("userData");
-      fs.writeFile(`${userData}\\config.json`, JSON.stringify(dataToWrite));
+    writeConfigTimeout = setTimeout(async () => {
+      try {
+        const tempFilePath = `${userData}\\config_temp.json`;
+        await fs.writeFile(tempFilePath, JSON.stringify(dataToWrite));
+
+        const configFilePath = `${userData}\\config.json`;
+        await move(tempFilePath, configFilePath, { overwrite: true });
+      } catch (e) {
+        console.log(e);
+      }
     }, 250);
   }
 }
