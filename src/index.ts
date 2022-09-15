96ebae81e1352a8711013c677d9a4fbac620e0e0
Tibor Ilić <tilic5@gmail.com>
2022-09-15 11:37:35 +0200

    noAsar, probably does nothing but can't hurt

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,8 @@ import electronLog from "electron-log";
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
+process.noAsar = true;
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   // eslint-disable-line global-require
