22f6fca0b2ea2db4365211bb6cef9d308ceda34f
Tibor Ilić <tilic5@gmail.com>
2022-09-22 23:01:19 +0200

    make the right panel fixed

--- src/PlayGame.tsx
+++ src/PlayGame.tsx
@@ -143,117 +143,149 @@ export default function PlayGame() {
     document.addEventListener("keyup", onKeyUp);
   });
 
-  const numEnabledMods = mods.filter(
+  const enabledMods = mods.filter(
     (iterMod) => iterMod.isEnabled || alwaysEnabledMods.find((mod) => mod.name === iterMod.name)
-  ).length;
+  );
 
   return (
     <div>
       <SaveGames isOpen={isShowingSavedGames} setIsOpen={setIsShowingSavedGames} />
-      <div id="presetSection">
-        <Tooltip placement="left" content="Create new preset by typing its name">
-          <span className="text-slate-100">Select or create preset:</span>
-        </Tooltip>
-        <Creatable
-          id="createOrSelectPreset"
-          value={defaultOption}
-          options={options}
-          onChange={onChange}
-          styles={selectStyle}
-          onCreateOption={(name) => newPresetMade(name)}
-        ></Creatable>
-        <div className="mt-5">
-          <span className="text-slate-100">Replace preset:</span>
-          <Select
-            id="replacePreset"
+      <div className="fixed">
+        <div id="presetSection">
+          <Tooltip placement="left" content="Create new preset by typing its name">
+            <span className="text-slate-100">Select or create preset:</span>
+          </Tooltip>
+          <Creatable
+            id="createOrSelectPreset"
+            value={defaultOption}
             options={options}
+            onChange={onChange}
             styles={selectStyle}
-            onChange={onReplaceChange}
-            value={null}
-          ></Select>
+            onCreateOption={(name) => newPresetMade(name)}
+          ></Creatable>
+          <div className="mt-5">
+            <span className="text-slate-100">Replace preset:</span>
+            <Select
+              id="replacePreset"
+              options={options}
+              styles={selectStyle}
+              onChange={onReplaceChange}
+              value={null}
+            ></Select>
+          </div>
+          <div className="mt-5">
+            <span className="text-slate-100">Delete preset:</span>
+            <Select
+              id="deletePreset"
+              options={options}
+              styles={selectStyle}
+              onChange={onDeleteChange}
+              value={null}
+            ></Select>
+          </div>
         </div>
-        <div className="mt-5">
-          <span className="text-slate-100">Delete preset:</span>
-          <Select
-            id="deletePreset"
-            options={options}
-            styles={selectStyle}
-            onChange={onDeleteChange}
-            value={null}
-          ></Select>
-        </div>
-      </div>
 
-      <div className="fixed right-[5%] bottom-[4%]">
-        <div className="text-center text-slate-100 mb-4">
-          {numEnabledMods > 0 && <>Enabled Mods: {numEnabledMods}</>}
-        </div>
-        <button
-          id="playGame"
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded h-14 w-36 m-auto"
-          onClick={() => playGameClicked()}
-        >
-          Play
-        </button>
+        <div className="mt-5 static">
+          <span className="text-slate-100">Filter:</span>
+          <span className="relative">
+            <input
+              id="filterInput"
+              type="text"
+              onChange={(e) => onFilterChange(e)}
+              value={filter}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            ></input>
 
-        <div className="mt-2">
+            <span className="absolute right-[0.65rem] top-8 text-gray-400">
+              <button onClick={() => clearFilter()}>
+                <FontAwesomeIcon icon={faXmark} />
+              </button>
+            </span>
+          </span>
+        </div>
+
+        <div className="fixed right-[5%] bottom-[4%] z-10">
+          <div className="text-center text-slate-100 mb-4">
+            {enabledMods.length > 0 && (
+              <>
+                <div className="make-tooltip-w-full">
+                  <Tooltip
+                    placement="top"
+                    content={enabledMods.map((mod) => (
+                      <div key={mod.path}>{mod.name.replace(".pack", "")}</div>
+                    ))}
+                  >
+                    Enabled Mods: {enabledMods.length}
+                  </Tooltip>
+                </div>
+              </>
+            )}
+          </div>
           <button
-            id="continueGame"
-            className="bg-green-600 border-green-500 border-2 hover:bg-green-700 text-white font-medium text-sm px-4 rounded  h-7 w-36 m-auto "
-            onClick={() => onContinueGameClicked()}
-            disabled={saves.length < 1}
+            id="playGame"
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded h-14 w-36 m-auto"
+            onClick={() => playGameClicked()}
           >
-            <span className="ml-[-25%]">Continue</span>
+            Play
           </button>
-          <button
-            id="showSaves"
-            type="submit"
-            className="absolute h-7 w-9 bottom-0 right-0 px-1 text-sm font-medium text-white bg-green-600 rounded-r-lg border border-green-500 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-            onClick={() => onShowSavedGamesClicked()}
-            disabled={saves.length < 1}
-          >
-            <svg
-              aria-hidden="true"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              overflow="visible"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+
+          <div className="mt-2">
+            <button
+              id="continueGame"
+              className="bg-green-600 border-green-500 border-2 hover:bg-green-700 text-white font-medium text-sm px-4 rounded h-7 w-36 m-auto "
+              onClick={() => onContinueGameClicked()}
+              disabled={saves.length < 1}
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7"></path>
-            </svg>
-          </button>
+              <div className="make-tooltip-w-full">
+                <Tooltip
+                  placement="left"
+                  content={(saves[0] && `Load ${saves[0].name}`) || "No saves found!"}
+                >
+                  <span className="ml-[-25%]">Continue</span>
+                </Tooltip>
+              </div>
+            </button>
+            <button
+              id="showSaves"
+              type="submit"
+              className="absolute h-7 w-9 bottom-0 right-0 px-1 text-sm font-medium text-white bg-green-600 rounded-r-lg border border-green-500 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              onClick={() => onShowSavedGamesClicked()}
+              disabled={saves.length < 1}
+            >
+              <div className="make-tooltip-w-full">
+                <Tooltip placement="left" content={(saves[0] && `Show all saves`) || "No saves found!"}>
+                  <svg
+                    aria-hidden="true"
+                    className="h-6 w-6"
+                    fill="none"
+                    stroke="currentColor"
+                    overflow="visible"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    ></path>
+                  </svg>
+                </Tooltip>
+              </div>
+            </button>
+          </div>
+        </div>
+
+        <div className="mt-6">
+          <OptionsDrawer />
+        </div>
+        <div className="mt-4">
+          <CompatScreen />
         </div>
       </div>
 
       <div className={"dark fixed w-80 mx-auto inset-x-0 bottom-[1%] " + (isUpdateAvailable ? "" : "hidden")}>
         <UpdateNotification downloadURL={downloadURL}></UpdateNotification>
-      </div>
-      <div className="mt-5 static">
-        <span className="text-slate-100">Filter:</span>
-        <span className="relative">
-          <input
-            id="filterInput"
-            type="text"
-            onChange={(e) => onFilterChange(e)}
-            value={filter}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          ></input>
-
-          <span className="absolute right-[0.65rem] top-8 text-gray-400">
-            <button onClick={() => clearFilter()}>
-              <FontAwesomeIcon icon={faXmark} />
-            </button>
-          </span>
-        </span>
-      </div>
-
-      <div className="mt-6">
-        <OptionsDrawer />
-      </div>
-      <div className="mt-4">
-        <CompatScreen />
       </div>
     </div>
   );
