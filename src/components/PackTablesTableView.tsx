8921d899c73eb02946147b0db130e0ee64b6a6a7
Tibor Ilić <tilic5@gmail.com>
2023-04-04 17:24:38 +0200

    resizable width for tree view in DB Viewer

--- src/components/PackTablesTableView.tsx
+++ src/components/PackTablesTableView.tsx
@@ -1,10 +1,5 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { getModsSortedByName, getModsSortedByHumanName, getModsSortedBySize } from "../modSortingHelpers";
-import { ActionMeta, SingleValue } from "react-select";
-import { createSelector } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
-import { useAppDispatch, useAppSelector } from "../hooks";
-import { Column, Row } from "@silevis/reactgrid";
+import React, { useEffect, useRef } from "react";
+import { useAppSelector } from "../hooks";
 import "@silevis/reactgrid/styles.css";
 import { getPackNameFromPath } from "../utility/packFileHelpers";
 import { AmendedSchemaField, SCHEMA_FIELD_TYPE } from "../packFileTypes";
@@ -15,245 +10,14 @@ import { HotTable } from "@handsontable/react";
 
 import { registerAllModules } from "handsontable/registry";
 
-type ModsMergeSorts = "Merge" | "MergeDesc" | "Pack" | "PackDesc" | "Name" | "NameDesc" | "Size" | "SizeDesc";
-type NumModsOptionType = {
-  value: string;
-  label: string;
-};
-type ExistingMergerOptionType = {
-  value: string;
-  label: string;
-};
-
 const PackTablesTableView = React.memo(() => {
-  const isDev = useAppSelector((state) => state.app.isDev);
   const currentDBTableSelection = useAppSelector((state) => state.app.currentDBTableSelection);
   const packsData = useAppSelector((state) => state.app.packsData);
 
   // console.log("packsData:");
   // console.log(packData);
 
-  const modsNotInDataSelector = createSelector(
-    (state: { app: AppState }) => state.app.currentPreset.mods,
-    (mods) => mods.filter((mod) => isDev || !mod.isInData)
-  );
-  const mods = useSelector(modsNotInDataSelector);
-
   const hotRef = useRef<HotTable>(null);
-
-  const allDependencyPacks =
-    mods
-      .map((mod) => mod.dependencyPacks)
-      .filter((depPack) => depPack != null)
-      .reduce((acc, val) => {
-        if (acc && val)
-          return acc?.concat(val.filter((depPack) => !acc.find((accPack) => accPack == depPack)));
-        return [];
-      }, [] as string[]) || [];
-
-  // console.log("allDependencyPacks", allDependencyPacks);
-
-  const mergerModsSelector = createSelector(
-    (state: { app: AppState }) => state.app.currentPreset.mods,
-    (mods) => mods.filter((mod) => mod.mergedModsData && mod.isEnabled)
-  );
-  const mergerMods = useSelector(mergerModsSelector);
-
-  const [useEnabledModsOnly, setUseEnabledModsOnly] = React.useState(true);
-  const [isHidingAlreadyMergedMods, setIsHidingAlreadyMergedMods] = React.useState(true);
-  const [isOpen, setIsOpen] = React.useState(true);
-  const [modsToMerge, setModsToMerge] = React.useState<Set<string>>(new Set<string>());
-  const [isSpinnerClosed, setIsSpinnerClosed] = React.useState(false);
-  const [modsMergeSort, setModsMergeSort] = React.useState("Size" as ModsMergeSorts);
-
-  let modsToUse = [...mods];
-
-  if (isHidingAlreadyMergedMods) {
-    modsToUse = modsToUse.filter((mod) =>
-      mergerMods.every((mergerMod) =>
-        mergerMod.mergedModsData?.every((mergedModData) => mergedModData.name != mod.name)
-      )
-    );
-  }
-
-  switch (modsMergeSort) {
-    case "Merge":
-      modsToUse = modsToUse.sort((firstMod, secondMod) => {
-        if (modsToMerge.has(firstMod.workshopId) && modsToMerge.has(secondMod.workshopId)) return 0;
-        if (modsToMerge.has(firstMod.workshopId)) return -1;
-        if (modsToMerge.has(secondMod.workshopId)) return 1;
-        return 0;
-      });
-      break;
-    case "MergeDesc":
-      modsToUse = modsToUse.sort((firstMod, secondMod) => {
-        if (modsToMerge.has(firstMod.workshopId) && modsToMerge.has(secondMod.workshopId)) return 0;
-        if (modsToMerge.has(firstMod.workshopId)) return 1;
-        if (modsToMerge.has(secondMod.workshopId)) return -1;
-        return 0;
-      });
-      break;
-    case "Pack":
-      modsToUse = getModsSortedByName(modsToUse);
-      break;
-    case "PackDesc":
-      modsToUse = getModsSortedByName(modsToUse).reverse();
-      break;
-    case "Name":
-      modsToUse = getModsSortedByHumanName(modsToUse);
-      break;
-    case "NameDesc":
-      modsToUse = getModsSortedByHumanName(modsToUse).reverse();
-      break;
-    case "Size":
-      modsToUse = getModsSortedBySize(modsToUse);
-      break;
-    case "SizeDesc":
-      modsToUse = getModsSortedBySize(modsToUse).reverse();
-      break;
-  }
-  modsToUse = modsToUse.filter((mod) => (!useEnabledModsOnly && mod) || mod.isEnabled);
-  if (useEnabledModsOnly) {
-    const filteredSet = new Set(
-      Array.from(modsToMerge).filter((workshopId) =>
-        modsToUse.some((modToUse) => modToUse.workshopId == workshopId)
-      )
-    );
-    if (filteredSet.size != modsToMerge.size) setModsToMerge(filteredSet);
-  }
-
-  const isPackProcessingDone = true; //!!packCollisions.packFileCollisions;
-
-  const onModToggled = useCallback(
-    (mod: Mod) => {
-      if (modsToMerge.has(mod.workshopId)) {
-        modsToMerge.delete(mod.workshopId);
-      } else {
-        modsToMerge.add(mod.workshopId);
-      }
-      setModsToMerge(new Set<string>(modsToMerge));
-    },
-    [modsToMerge]
-  );
-
-  const modsWithoutDependencies = useCallback(
-    (mods: Mod[]) => {
-      return mods.filter(
-        (mod) =>
-          (!mod.dependencyPacks || mod.dependencyPacks.length < 1) &&
-          !allDependencyPacks.some((packName) => packName == mod.name)
-      );
-    },
-    [mods]
-  );
-
-  const onSelectNumModsChange = useCallback(
-    (newValue: SingleValue<NumModsOptionType>, actionMeta: ActionMeta<NumModsOptionType>) => {
-      if (!newValue) return;
-      console.log(`label: ${newValue.label}, value: ${newValue.value}, action: ${actionMeta.action}`);
-      if (actionMeta.action === "select-option") {
-        // setModsToMerge(
-        //   new Set<string>(
-        //     modsWithoutDependencies(modsToUse)
-        //       .slice(0, newValue.value)
-        //       .map((mod) => mod.workshopId)
-        //   )
-        // );
-      }
-    },
-    [modsToUse]
-  );
-  const onSelectExistingMergerChange = useCallback(
-    (newValue: SingleValue<ExistingMergerOptionType>, actionMeta: ActionMeta<ExistingMergerOptionType>) => {
-      if (!newValue) return;
-      console.log(`label: ${newValue.label}, value: ${newValue.value}, action: ${actionMeta.action}`);
-      if (actionMeta.action === "select-option") {
-        const mergerMod = mergerMods.find((mergerMod) => mergerMod.name == newValue.value);
-        if (!mergerMod || !mergerMod.mergedModsData) return;
-        const mergedData = mergerMod.mergedModsData;
-        setModsToMerge(
-          new Set<string>(
-            modsWithoutDependencies(modsToUse)
-              .filter((mod) => mergedData.some((mergedModData) => mergedModData.name == mod.name))
-              .map((mod) => mod.workshopId)
-          )
-        );
-      }
-    },
-    [mergerMods]
-  );
-
-  const options: NumModsOptionType[] = useMemo(
-    () =>
-      mods.map((mod) => {
-        return { value: mod.path, label: mod.name + ((mod.humanName != "" && mod.humanName) || "") };
-      }),
-    [mods]
-  );
-
-  const mergerOptions = useMemo<ExistingMergerOptionType[]>(
-    () =>
-      mergerMods.map((mod) => {
-        return { value: mod.name, label: mod.name };
-      }),
-    [mergerMods]
-  );
-
-  const mergeMods = useCallback(() => {
-    if (modsToMerge.size < 1) return;
-    const modsToMergeArray = Array.from(modsToMerge);
-    window.api?.mergeMods(
-      mods.filter((mod) => modsToMergeArray.some((modToMergeId) => modToMergeId == mod.workshopId))
-    );
-    setIsOpen(false);
-  }, [modsToMerge]);
-
-  const toggleMergeSorting = useCallback(() => {
-    if (modsMergeSort == "Merge") setModsMergeSort("MergeDesc");
-    else setModsMergeSort("Merge");
-  }, [modsMergeSort]);
-  const toggleNameSorting = useCallback(() => {
-    if (modsMergeSort == "Name") setModsMergeSort("NameDesc");
-    else setModsMergeSort("Name");
-  }, [modsMergeSort]);
-  const toggleSizeSorting = useCallback(() => {
-    if (modsMergeSort == "Size") setModsMergeSort("SizeDesc");
-    else setModsMergeSort("Size");
-  }, [modsMergeSort]);
-  const togglePackSorting = useCallback(() => {
-    if (modsMergeSort == "Pack") setModsMergeSort("PackDesc");
-    else setModsMergeSort("Pack");
-  }, [modsMergeSort]);
-
-  const onMergeRightClick = useCallback(() => {
-    setModsToMerge(new Set<string>());
-  }, [modsMergeSort]);
-
-  const [modFilter, setModFilter] = useState("");
-  const onFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setModFilter(e.target.value);
-  };
-
-  const clearFilter = () => {
-    setModFilter("");
-  };
-
-  useEffect(() => {
-    const onKeyDown = (e: KeyboardEvent) => {
-      if (e.ctrlKey && e.key === "f") {
-        const modMergingFilter = document.getElementById("modMergingFilter");
-        modMergingFilter?.focus();
-        // e.stopPropagation();
-        e.stopImmediatePropagation();
-      }
-    };
-
-    document.addEventListener("keydown", onKeyDown);
-
-    return () => {
-      document.removeEventListener("keydown", onKeyDown);
-    };
-  });
 
   useEffect(() => {
     if (!hotRef || !hotRef.current) return;
@@ -304,22 +68,6 @@ const PackTablesTableView = React.memo(() => {
     return <></>;
   }
 
-  const getColumns = (): Column[] =>
-    Array.from(currentSchema.fields.keys()).map((i) => {
-      return {
-        columnId: i,
-      };
-    });
-
-  const headerRow: Row = {
-    rowId: "header",
-    cells: currentSchema.fields.map((field) => {
-      {
-        return { type: "header", text: field.name };
-      }
-    }),
-  };
-
   const chunkedTable =
     (packFile.schemaFields &&
       packFile.schemaFields.reduce<AmendedSchemaField[][]>((resultArray, item, index) => {
@@ -334,17 +82,6 @@ const PackTablesTableView = React.memo(() => {
         return resultArray;
       }, [])) ||
     [];
-
-  const rows = [
-    headerRow,
-    ...chunkedTable.map<Row>((fieldsRow, idx) => ({
-      rowId: idx,
-      cells: fieldsRow.map((field) => ({
-        type: "text",
-        text: field.resolvedKeyValue,
-      })),
-    })),
-  ];
 
   const data = chunkedTable.map((row) =>
     row.map((cell) => {
