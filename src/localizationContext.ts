d36b356808976698db660d173105353218cbe554
Milos <milos192@users.noreply.github.com>
2025-01-07 14:53:49 +0100

    Add a search input to the Saved Games modal

--- src/localizationContext.ts
+++ src/localizationContext.ts
@@ -1,7 +1,11 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { default as enTranslation } from "../locales/en/translation.json";
 // we just need the loc keys
 export const staticTextIds: Record<string, string | number> = enTranslation;
 
 const context = createContext({});
 export default context;
+
+export const useLocalizations = () => {
+  return useContext(context) as typeof enTranslation;
+}
