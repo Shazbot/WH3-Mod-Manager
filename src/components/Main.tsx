d495073c23f09fae8fa816929b16eaa97bacbcc3
Tibor Ilić <tilic5@gmail.com>
2025-10-03 14:37:29 +0200

    populate DB table data for nodes that use it

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,17 +13,18 @@ const Main = (props: MainProps) => {
   const currentTab = useAppSelector((state) => state.app.currentTab);
   return (
     <>
-      {(currentTab == "categories" && <NodeEditor></NodeEditor>) || (
-        <div className="grid grid-cols-12 text-white max-w-[100rem] mx-auto">
-          <div className="col-span-10">
-            <ModRows scrollElement={props.scrollElement} />
+      {(currentTab == "nodeEditor" && <NodeEditor></NodeEditor>) ||
+        (currentTab == "categories" && <Categories></Categories>) || (
+          <div className="grid grid-cols-12 text-white max-w-[100rem] mx-auto">
+            <div className="col-span-10">
+              <ModRows scrollElement={props.scrollElement} />
+            </div>
+            <div className="ml-3 col-span-2 relative">
+              <Sidebar />
+            </div>
+            <ModTagPicker></ModTagPicker>
           </div>
-          <div className="ml-3 col-span-2 relative">
-            <Sidebar />
-          </div>
-          <ModTagPicker></ModTagPicker>
-        </div>
-      )}
+        )}
     </>
   );
 };
